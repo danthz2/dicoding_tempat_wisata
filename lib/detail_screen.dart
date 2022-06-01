1183818b3d7c24f1868113493366999914b78bf9
faqih <faqihsidj@gmail.com>
2022-06-01 16:29:18 +0700

    Codelab 4: Pengembangan untuk Web

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -8,6 +8,185 @@ class DetailScreen extends StatelessWidget {
   String option = "Op1";
   @override
   Widget build(BuildContext context) {
+    return LayoutBuilder(builder: ((context, constraints) {
+      if (constraints.maxWidth > 800) {
+        return DetailWebPage(place: place);
+      } else {
+        return DetailMobilePage(place: place);
+      }
+    }));
+  }
+}
+
+class DetailWebPage extends StatelessWidget {
+  final TourismPlace place;
+  DetailWebPage({required this.place});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Wisata Bandung"),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Column(
+                    children: [
+                      Image.asset(place.imageAsset),
+                      Row(
+                        children: place.imageUrls.map((e) {
+                          return Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: Image.network(
+                                e,
+                                height: 100,
+                              ),
+                            ),
+                          );
+                        }).toList(),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Card(
+                    child: Column(
+                      children: [
+                        Text(place.name),
+                        Row(
+                          children: [
+                            Icon(Icons.calendar_today),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(place.openDays)
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Icon(Icons.access_time),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(place.openTime)
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Icon(Icons.monetization_on),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(place.ticketPrice)
+                          ],
+                        ),
+                        Text(place.description),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [Text("1")],
+            )
+            // Row(
+            //   children: [
+            //     Image.asset(place.imageAsset),
+            //     Column(
+            //       mainAxisAlignment: MainAxisAlignment.spaceAround,
+            //       children: [
+            //         Row(
+            //           children: [
+            //             Icon(Icons.calendar_today),
+            //             SizedBox(
+            //               height: 10,
+            //             ),
+            //             Text(place.openDays)
+            //           ],
+            //         ),
+            //         Row(
+            //           children: [
+            //             Icon(Icons.access_time),
+            //             SizedBox(
+            //               height: 10,
+            //             ),
+            //             Text(place.openTime)
+            //           ],
+            //         ),
+            //         Row(
+            //           children: [
+            //             Icon(Icons.monetization_on),
+            //             SizedBox(
+            //               height: 10,
+            //             ),
+            //             Text(place.ticketPrice)
+            //           ],
+            //         ),
+            //         Container(
+            //           padding: EdgeInsets.all(16),
+            //           child: Text(
+            //             place.description,
+            //             textAlign: TextAlign.center,
+            //           ),
+            //         ),
+            //       ],
+            //     ),
+            //   ],
+            // ),
+            // SizedBox(
+            //   height: 15,
+            // ),
+            // Text(
+            //   place.name,
+            //   style: TextStyle(
+            //     fontSize: 30,
+            //     fontWeight: FontWeight.bold,
+            //   ),
+            // ),
+            // SizedBox(
+            //   height: 10,
+            // ),
+            // Container(
+            //   height: 150,
+            //   child: ListView(
+            //     scrollDirection: Axis.horizontal,
+            //     children: [
+            //       ...place.imageUrls.map((e) {
+            //         return Padding(
+            //           padding: const EdgeInsets.all(8.0),
+            //           child: ClipRRect(
+            //             borderRadius: BorderRadius.circular(10),
+            //             child: Image.network(e),
+            //           ),
+            //         );
+            //       }).toList()
+            //     ],
+            //   ),
+            // ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class DetailMobilePage extends StatelessWidget {
+  const DetailMobilePage({
+    Key? key,
+    required this.place,
+  }) : super(key: key);
+
+  final TourismPlace place;
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -7,55 +7,143 @@ class MainScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Wisata Bandung"),
+        title:
+            Text("Wisata Bandung, Size : ${MediaQuery.of(context).size.width}"),
       ),
-      body: ListView.builder(
-        itemCount: tourismPlaceList.length,
-        itemBuilder: (builder, index) {
+      body: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            if (constraints.maxWidth >= 1200) {
+              return TourismPlaceGrid(
+                gridCount: 6,
+              );
+            } else if (constraints.maxWidth >= 600) {
+              return TourismPlaceGrid(gridCount: 4);
+            } else {
+              return TourismPlaceList();
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class TourismPlaceGrid extends StatelessWidget {
+  int gridCount;
+  TourismPlaceGrid({required this.gridCount});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      crossAxisSpacing: 16,
+      mainAxisSpacing: 16,
+      crossAxisCount: gridCount,
+      children: tourismPlaceList.map(
+        (place) {
+          int index = 0;
           return InkWell(
             onTap: () => Navigator.push(
               context,
               MaterialPageRoute(
-                builder: (context) => DetailScreen(
-                  place: tourismPlaceList[index],
-                ),
+                builder: (context) {
+                  return DetailScreen(
+                    place: place,
+                  );
+                },
               ),
             ),
             child: Card(
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
                   Expanded(
-                      flex: 1,
-                      child: Image.asset(
-                        tourismPlaceList[index].imageAsset,
-                      )),
-                  Expanded(
-                    flex: 2,
-                    child: Padding(
-                      padding: EdgeInsets.all(8),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Text(
-                            tourismPlaceList[index].name,
-                            style: TextStyle(fontSize: 16),
-                          ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Text(tourismPlaceList[index].location),
-                        ],
+                    child: Image.asset(
+                      place.imageAsset,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8.0),
+                    child: Text(
+                      place.name,
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                  )
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8.0, bottom: 8.0),
+                    child: Text(
+                      place.location,
+                    ),
+                  ),
                 ],
               ),
             ),
           );
         },
-      ),
+      ).toList(),
+    );
+  }
+}
+
+class TourismPlaceList extends StatelessWidget {
+  const TourismPlaceList({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: tourismPlaceList.length,
+      itemBuilder: (builder, index) {
+        return InkWell(
+          onTap: () => Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => DetailScreen(
+                place: tourismPlaceList[index],
+              ),
+            ),
+          ),
+          child: Card(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                    flex: 1,
+                    child: Image.asset(
+                      tourismPlaceList[index].imageAsset,
+                    )),
+                Expanded(
+                  flex: 2,
+                  child: Padding(
+                    padding: EdgeInsets.all(8),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          tourismPlaceList[index].name,
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(tourismPlaceList[index].location),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
